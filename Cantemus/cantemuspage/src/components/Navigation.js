4b1078e8d025c3f2873eae257f834b2d9f642404
cryingshadow <cryingshadow@web.de>
2020-12-27 14:20:19 +0100

    only include used components from react bootstrap

--- Cantemus/cantemuspage/src/components/Navigation.js
+++ Cantemus/cantemuspage/src/components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Nav, Navbar} from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default class Navigation extends React.Component {
   render() {
